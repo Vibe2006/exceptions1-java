1c7dece77effd48e2fd908d29e8079242357dfac
EmpresaGlow <glow.etec@gmail.com>
2026-07-10 16:42:14 -0300

    bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,8 @@ public class Program {
 
 	public static void main(String[] args) throws ParseException{
 		
+		//delegar lógicas para classes significa deixar a classe resposável por algo fazer o algo
+		
 		Scanner sc = new Scanner(System.in);
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -31,24 +33,23 @@ public class Program {
 			
 			System.out.println();
 			System.out.println("Enter date to update the reservation: ");
-			System.out.println("Check-in date (dd/MM/yyyy): ");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
 			
-			Date now = new Date();
 			
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}
-			else if(!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
 			else {
-				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation: " + reservation);
 			}
+			
+			
+			
 			
 			
 		}
